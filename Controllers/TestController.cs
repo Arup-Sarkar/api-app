8db3df449a79d29c184cc6d7fb7d344b6035a819
Arup-Sarkar <arup.sarkar943@gmail.com>
2019-08-10 10:21:05 +0530

    no Change

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
 namespace api.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]/[action]")]
     public class TestController
     {
+
         //this is just for test only
     }
 }
