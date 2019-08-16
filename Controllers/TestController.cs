937fec083ff1e98b3a575a989c0f08c873f6babb
Arup-Sarkar <arup.sarkar943@gmail.com>
2019-08-16 10:35:58 +0530

    Last mysql local server added

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,13 +1,24 @@
+using System.Net.Cache;
 using System;
 using System.Web;
 using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using Newtonsoft.Json;
+using MySql.Data.MySqlClient;
+using System.Linq;
 namespace api.Controllers
 {
     [ApiController]
     [Route("api/[controller]/[action]")]
     public class TestController
     {
-
-        //this is just for test only
+        private readonly string constring=@"Data Source=localhost;Initial Catalog=arupdb;user=arup;password=password;";
+        [HttpGet]
+        public string Testdata(){
+            using(var con=new MySqlConnection(constring)){
+                var data=con.Query("select * from test").ToList();
+                return JsonConvert.SerializeObject(data);
+            }
+        }
     }
 }
